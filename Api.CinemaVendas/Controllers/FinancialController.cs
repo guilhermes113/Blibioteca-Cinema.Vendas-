4d9068ff726ee012801164ccb0732b4797394ca7
GUILHERME SANCHES DA ROCHA <guilherme.srocha11@senacsp.edu.br>
2022-09-20 21:54:41 -0300

    Criando-Ultimas-Crontrollers

---
+++ Api.CinemaVendas/Controllers/FinancialController.cs
@@ -0,0 +1,28 @@
+﻿using CinemaVendas.Core.Services;
+using CinemaVendas.Core.Services.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.CinemaVendas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FinancialController : ControllerBase
+    {
+        private readonly IFinancialsService _financialsService;
+        public FinancialController(IFinancialsService financialsService)
+        {
+            _financialsService = financialsService;
+        }
+        public IActionResult GetTotalSold()
+        {
+            return Ok(_financialsService.GetTotalSold());
+        }
+        
+        
+    }
+}
---
+++ Api.CinemaVendas/Controllers/FoodController.cs
@@ -0,0 +1,26 @@
+﻿using CinemaVendas.Core.Services;
+using CinemaVendas.Core.Services.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.CinemaVendas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodController : ControllerBase
+    {
+        private readonly IFoodService _foodService;
+        public FoodController(IFoodService foodservice)
+        {
+            _foodService = foodservice;
+        }
+        public IActionResult GetTicket()
+        {
+            return Ok(_foodService.GetAllSold());
+        }
+    }
+}
